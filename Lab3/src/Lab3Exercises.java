71f49130e0834a52b8cf8c58c4da8310a442655b
4010d1 <4010d1@nispero.ece.uprm.edu>
2017-02-08 15:09:12 -0400

    Changes in JFrame title, location and size.

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,16 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame("Wirber");
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		Dimension sizeofscreen = Toolkit.getDefaultToolkit().getScreenSize();
+		sizeofscreen.getHeight();
+		sizeofscreen.getWidth();
+		myFrame.setLocation(sizeofscreen.width/2, sizeofscreen.height/2);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
